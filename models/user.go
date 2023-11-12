3708c67ea64adcee4b57807f142bb6344044356b
Yash <ypt7895@gmail.com>
2023-11-13 01:16:47 +0530

    registration input and functions to add user in db

--- models/user.go
+++ models/user.go
@@ -1,10 +1,42 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"html"
+	"mksc_api/database"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"size:255;not null;unique" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"-"`
 	ApiKey   string `gorm:"size:255;not null;unique;" json:"-"`
+}
+
+func (user *User) Save() (*User, error) {
+	err := database.Database.Create(&user).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return user, nil
+}
+
+func (user *User) BeforeSave(*gorm.DB) error {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	apiKeyHash, err := bcrypt.GenerateFromPassword([]byte(user.ApiKey), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user.ApiKey = string(apiKeyHash)
+	user.Password = string(passwordHash)
+	user.Username = html.EscapeString(strings.TrimSpace(user.Username))
+	return nil
 }
