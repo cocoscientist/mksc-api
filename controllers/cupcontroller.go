80d03951761fb651a372ed2a498a3bc235bb7e22
Yash <ypt7895@gmail.com>
2023-11-15 01:23:39 +0530

    added cups object

---
+++ controllers/cupcontroller.go
@@ -0,0 +1,64 @@
+package controllers
+
+import (
+	"mksc_api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CupInput struct {
+	Name  string `json:"name" binding:"required"`
+	Image string `json:"image" binding:"required"`
+}
+
+func AddCup(context *gin.Context) {
+	token := context.Request.Header["Key"][0]
+
+	user, erro := models.FindUserByApiKey(token)
+
+	if erro != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": erro.Error()})
+		return
+	}
+
+	println(user.Username)
+
+	var input CupInput
+
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	cup := models.Cup{
+		Name:  input.Name,
+		Image: input.Image,
+	}
+
+	savedCup, err := cup.SaveCup()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"cup": savedCup})
+}
+
+func GetAllCups(context *gin.Context) {
+	token := context.Request.Header["Key"][0]
+
+	user, erro := models.FindUserByApiKey(token)
+
+	if erro != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": erro.Error()})
+		return
+	}
+
+	println(user.Username)
+
+	cups := models.FindAllCups()
+
+	context.JSON(http.StatusAccepted, cups)
+}
---
+++ models/cup.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"mksc_api/database"
+
+	"gorm.io/gorm"
+)
+
+type Cup struct {
+	gorm.Model
+	Name   string `gorm:"size:255;not null;unique" json:"name"`
+	Image  string `gorm:"not null;unique" json:"image"`
+	Tracks []Track
+}
+
+func (cup *Cup) SaveCup() (*Cup, error) {
+	err := database.Database.Create(&cup).Error
+	if err != nil {
+		return &Cup{}, err
+	}
+	return cup, nil
+}
+
+func FindCupByName(name string) (Cup, error) {
+	var curCup Cup
+	erro := database.Database.Where("name=?", name).Find(&curCup).Error
+	if erro != nil {
+		return Cup{}, erro
+	}
+	return curCup, nil
+}
+
+func FindAllCups() []Cup {
+	var cups []Cup
+	result := database.Database.Find(&cups)
+	if result.Error != nil {
+		return nil
+	}
+	return cups
+}
